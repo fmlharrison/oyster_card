0564560181fa3630a3cee287e82d5074db73d4bb
Dagmara <daga.sz@hotmail.com>
2016-10-03 16:00:31 +0100

    max balance added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize(balance=0)
     @balance = balance
   end
 
   def top_up(value)
+    fail "Card limit (£#{Oystercard::MAX_BALANCE}) reached" if balance + value > MAX_BALANCE
     @balance += value
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,8 @@ describe Oystercard do
     card.top_up(5)
     expect(card.balance).to eq 5
   end
+
+  it "has a max balance of 90" do
+    expect{card.top_up(91)}.to raise_error("Card limit (£#{Oystercard::MAX_BALANCE}) reached")
+  end
 end
